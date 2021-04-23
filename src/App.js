3397468404dc149a28e2454bf72078a807569b73
github username <avelonpang@gmail.com>
2021-04-22 23:20:22 -0500

    added api id and keys

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
-import logo from './logo.svg';
+import React, {useEffect, useState} from "react";
 import './App.css';
 
-function App() {
+const App = () => {
+
+  const APP_ID = '59f653d6';
+  const APP_KEY = '96e02fae3d7a4a0146afe8d580454fe5';	
+  const exampleRequest = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+
+  });
+
+
   return (
     <div className="App">
-      <header>
-      </header>
+      <form className="search-form">
+      <input className="search-bar" type="text"/>
+      <button  className="search-button" type="submit"> Search </button>
+      </form>
+      <h1 onClick={() => setCounter(counter +1)}>{counter}</h1> 
     </div>
   );
-}
+};
 
 export default App;
