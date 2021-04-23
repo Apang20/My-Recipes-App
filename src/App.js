f8a9085a9d729cf6709e14e6a89152c46ac655cc
github username <avelonpang@gmail.com>
2021-04-23 15:08:19 -0500

    fetch api

--- src/App.js
+++ src/App.js
@@ -5,13 +5,17 @@ const App = () => {
 
   const APP_ID = '59f653d6';
   const APP_KEY = '96e02fae3d7a4a0146afe8d580454fe5';	
-  const exampleRequest = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
 
-  const [counter, setCounter] = useState(0);
+  useEffect ( () => {
+    getRecipes()
+  }, []);
 
-  useEffect(() => {
-    console.log("Effect has ran");
-  });
+  const getRecipes = async () => {
+    const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`)
+    const data = await response.json(); 
+    console.log(data); 
+
+  }
 
 
   return (
@@ -20,7 +24,6 @@ const App = () => {
       <input className="search-bar" type="text"/>
       <button  className="search-button" type="submit"> Search </button>
       </form>
-      <h1 onClick={() => setCounter(counter +1)}>{counter}</h1> 
     </div>
   );
 };
